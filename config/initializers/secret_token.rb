4a50b1cef0871f1daf2da355f947395f44c68e13
Denis Krivostchokov <densomart@gmail.com>
2012-04-06 14:15:00 +0700

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Agile::Application.config.secret_token = '867b3d0afe5aef9a856101e47195d62c74be16b0d20bfe83a23aaba22351ccf33c6c715e23bad0fed54ad9191b2d3fe20e5afd216c4ae29d0b605b8c2192d714'
